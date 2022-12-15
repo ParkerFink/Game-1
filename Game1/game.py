8ddc1944be01e76d46334b2adbde449bae49bfd5
FinkParker <parkeririshgod@gmail.com>
2022-12-14 20:29:46 -0500

    movement broky

--- Game1/game.py
+++ Game1/game.py
@@ -6,12 +6,15 @@ window = pygame.display.set_mode((800, 500))
 pygame.display.set_caption("Game 1")
 
 
-class Character():
-    health = 100
-    armor = 100
-    player = pygame.image.load('textures/images.jpg')
-    playerX = 100
-    playerY = 100 
+playerIMG = pygame.image.load('textures/player.jpg')
+playerX = 100
+playerY = 100
+
+
+
+
+def playerSpawn(x,y):
+    window.blit(playerIMG, (x, y))   
 
 
 
@@ -21,6 +24,20 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-        
     
+    pressed = pygame.key.get_pressed()
+    if pressed [pygame.K_LEFT]:
+        playerX -= .1
+
+    if pressed [pygame.K_RIGHT]:
+        playerX += .1
+
+    if pressed [pygame.K_UP]:
+        playerY -= .1
+
+    if pressed [pygame.K_DOWN]:
+        playerY += .1
+
+    playerSpawn(playerX, playerY)
     pygame.display.update()
+
